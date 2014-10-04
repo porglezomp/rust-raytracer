4d8daf85f17a6115eee44ebf68bf556eed9dc923
Caleb Jones <bionicle.dragon@gmail.com>
2014-10-03 22:51:06 -0400

    Render pixels concurrently

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,52 @@
 extern crate lodepng;
 
+use std::comm;
+
+#[deriving(Clone)]
+struct Point {x: u32, y: u32}
+#[deriving(Clone)]
 struct Pixel {r: u8, g: u8, b: u8}
 
-static h : u32 = 10;
-static w : u32 = 10;
+static h : u32 = 512;
+static w : u32 = 512;
 static pixel_count : uint = (w*h) as uint;
 
 fn main() {
-    let path = &Path::new("test.png");
-
-    let pixel_data = [Pixel {r: 0, g: 0, b: 0}, ..pixel_count];
     let mut data = [0u8, ..pixel_count*3u];
-    assert!(pixel_data.len()*3 == data.len());
-    for (i, p) in pixel_data.iter().enumerate() {
-        data[i*3 + 0] = p.r;
-        data[i*3 + 1] = p.g;
-        data[i*3 + 2] = p.b;
+    let (tx, rx) : (Sender<(Point, Pixel)>, Receiver<(Point, Pixel)>) = comm::channel();
+    let mut points : Vec<Point> = Vec::with_capacity(pixel_count);
+
+    for x in range(0, w) {
+        for y in range(0, h) {
+            points.push(Point {x: x, y: y});
+        }
     }
+    
+    for p in points.iter() {
+        let proc_tx = tx.clone();
+        let point = p.clone();
+        spawn(proc() {
+            proc_tx.send((point, render(point)));
+        });
+    }
+    
+    for _ in range(0, pixel_count) {
+        let (point, pixel) = rx.recv();
+        let index : uint = ((point.x + point.y * w) * 3) as uint;
+        data[index + 0] = pixel.r;
+        data[index + 1] = pixel.g;
+        data[index + 2] = pixel.b;
+    }
+
+    let path = &Path::new("test.png");
     match lodepng::encode_file(path, data, w, h, lodepng::LCT_RGB, 8) {
         Err(e) => fail!("Error writing: {}", e),
         Ok(_)  => (),
     }
+}
+
+fn render(point: Point) -> Pixel {
+    Pixel {r: (point.x % 256) as u8,
+           g: (point.y % 256) as u8,
+           b: 128}
 }
