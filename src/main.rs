0a55070db8f95da11bcf59ea6a536e8550a2025b
Caleb Jones <bionicle.dragon@gmail.com>
2014-10-03 20:06:28 -0400

    Base number of pixels on the width and the height
    Have to multiply by 3u instead of 3, because of compiler bugs.

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ struct Pixel {r: u8, g: u8, b: u8}
 
 static h : u32 = 10;
 static w : u32 = 10;
-static pixel_count : uint = 100;
+static pixel_count : uint = (w*h) as uint;
 
 fn main() {
     let path = &Path::new("test.png");
 
     let pixel_data = [Pixel {r: 0, g: 0, b: 0}, ..pixel_count];
-    let mut data = [0u8, ..pixel_count*3];
+    let mut data = [0u8, ..pixel_count*3u];
     assert!(pixel_data.len()*3 == data.len());
     for (i, p) in pixel_data.iter().enumerate() {
         data[i*3 + 0] = p.r;
